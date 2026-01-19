075abf01e4ba878584f4b02736d46bd00d45afc9
Vanessa Liz <vanessalizsousa1996@gmail.com>
2026-01-19 20:34:42 -0300

    19/01/26

---
+++ atividade08.py
@@ -0,0 +1,15 @@
+#criar um jogo de tabuleiro
+
+def drawboard(kamal):
+    kamal = int(kamal)
+    i = 0
+    ho = "--- "
+    ve = "|   "
+    ho = ho * kamal
+    ve = ve * (kamal+1)
+    while i < kamal+1:
+        print ho
+        if not (i == kamal):
+            print ve
+        i += 1
+    
